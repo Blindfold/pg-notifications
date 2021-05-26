9f6f720ad2e0a995181716102e73707154ad03e5
Denis Kozlov <denis.kozlov@quantori.com>
2021-05-26 12:53:49 +0300

    move parameters to environment vars

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import pg_channels
 import pika
 import os
 
-pgc = pg_channels.connect(host='localhost', database='postgres', password="password", user="postgres")
-mq = pika.BlockingConnection(pika.ConnectionParameters(os.environ.get('RABBITMQ-HOST')))
+pgc = pg_channels.connect(host=os.environ.get('POSTGRES-HOST'),
+                          database=os.environ.get('POSTGRES-DB'),
+                          user=os.environ.get('POSTGRES-USER'),
+                          password = os.environ.get('POSTGRES-PASSWORD'))
+mq = pika.BlockingConnection(pika.ConnectionParameters(host=os.environ.get('RABBITMQ-HOST') or 'localhost'))
 
 mq_channel = mq.channel()
-mq_channel.queue_declare(queue='test')
-pgc.listen('test')
+mq_queue_name = os.environ.get('RABBITMQ-QUEUE') or 'test'
+mq_channel.queue_declare(queue=mq_queue_name)
+pgc.listen(os.environ.get('POSTGRES-NOTIFY-CHANNEL'))
 
 for event in pgc.events():
     print(" [x] Received %r" % event.payload)
-    mq_channel.basic_publish(exchange='', routing_key='test', body=event.payload)
-    pgc.notify(event.channel + '_reply', event.payload)
+    mq_channel.basic_publish(exchange='', routing_key=mq_queue_name, body=event.payload)
